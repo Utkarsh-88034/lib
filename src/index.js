b0dc9ed1913f8af8f5c00e1d9607c1360e652b96
utkarsh <robbin7838@gmail.com>
2022-01-06 23:07:05 +0530

    component library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect, useMemo } from 'react'
 import styles from './styles.module.css'
 
 export const ExampleComponent = ({ text }) => {
+  useEffect(() => {
+    console.log('hello')
+  })
+  const newText = useMemo(() => {
+    return 'noice'
+  }, [])
+  console.log(newText)
   return <div className={styles.test}>Example Component: {text}</div>
 }
